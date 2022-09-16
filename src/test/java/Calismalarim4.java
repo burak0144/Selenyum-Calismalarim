bd58c45fc5588f499a625cba4ef776187185e768
burak <karanfilkaranfil822@gmail.com>
2022-09-16 20:59:24 +0300

    "16.09.22"

--- src/test/java/Calismalarim4.java
+++ src/test/java/Calismalarim4.java
@@ -1,2 +1,12 @@
 public class Calismalarim4 {
+//  Launch browser
+// Navigate to url 'http://automationexercise.com'
+//  Verify that home page is visible successfully
+// Click on 'Signup / Login' button
+// Verify 'Login to your account' is visible
+// Enter correct email address and password
+// Click 'login' button
+// Verify that 'Logged in as username' is visible
+// Click 'Logout' button
+//. Verify that user is navigated to login page
 }
