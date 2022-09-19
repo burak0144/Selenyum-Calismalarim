1a89d202e80f9ab35bc56f23ee392324cf25777d
burak <karanfilkaranfil822@gmail.com>
2022-09-19 17:32:19 +0300

    "19.09.22"

--- src/test/java/practice.java
+++ src/test/java/practice.java
@@ -1,2 +1,19 @@
 public class practice {
+    /*
+    Selenium suanakadar 4 tool cıkardı, Selenium RC, Selenium Webdriver,Selenium IDE, Selenium Grid. Selenium WebDriver-->Selenium2,Selenium3,Selenium4..
+ Selenium neden cok tercih ediliyor? En büyük sebebi ucretsiz olması, ikincisi coğu platformlarda calısabilmesi.farklı Dillerdeki browserlari destekleyebilmesi
+mobile testi destekler
+Agile ve Continuous Delivery ile entegre
+Kullanici sayisi fazla
+Genix plugin ve extention kütüphaneleri mevcut
+Dezavantajları
+Bir kod bilgisi gerektirir
+Sadece web tabanlı uygulamalarda kullanılır. (mobile test için appium kullaniliyor)
+Görüntü karsılastırma yok
+Teknik destek yok
+Raporlama özelliği yok
+Grafik, captchas, barkod ve sekiller ile kullanılamaz.
+     */
+
 }
+
