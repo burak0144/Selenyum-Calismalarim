40d821acc5567944ae9c5f0bb6ba80b16314ea00
burak <karanfilkaranfil822@gmail.com>
2022-09-21 07:55:21 +0300

    "19.09.22"

--- src/test/java/practise_Elf/C05.java
+++ src/test/java/practise_Elf/C05.java
@@ -1,4 +1,10 @@
 package practise_Elf;
 
 public class C05 {
+    // ...Exercise5...
+    // @BeforeClass'in icine driver'i kuralim
+    // maximize edip tum web elementler yuklenene kadar 10sn bekletelim
+    // Google'a gidelim ve sayfa basliginin "Google" icerdigini dogrulayalim
+    // Amazon'a gidelim ve url in www.amazon.com icerip icermedigini dogrulayalim
+    // @AfterClass to driver'i kapatalim
 }
