3724ef270549a794e15e3029fb6381125e9d9802
burak <karanfilkaranfil822@gmail.com>
2022-09-21 07:15:44 +0300

    "19.09.22"

---
+++ src/test/java/practise_Elf/C04.java
@@ -0,0 +1,42 @@
+package practise_Elf;
+
+public class C04 {
+     /* note:
+    JavaScriptExecutor  nedir?
+    JavaScriptExecutor, JavaScript'i Selenium Web sürücüsü araciligiyla calistirmaya yardimci olan bir arayuzdur.
+    JavaScriptExecutor, javascript'i secilen pencerede veya gecerli sayfada calistirmak icin "executescript" ve "executeAsyncScript"
+    olmak uzere iki yontem sunar.
+
+    JavaScriptExecutor'a neden ihtiyacimiz var?
+    Selenium Webdriver'da, bir web sayfasindaki islemleri tanimlamak ve gerceklestirmek icin
+    XPath,CSS vb. gibi konum belirliyiciler kullanilir.
+    Bu konum belirliyicilerin calismamasi durumunda JavaScriptExecutor'i kullanabilirsiniz.
+    Bir web ogesinde istenen bir islemi gerceklestirmek icin JavaScriptExecutor'i kullanabilirsiniz
+    Selenium javaScriptExecutor'i destekler.Ekstra bir eklenti veya eklentiye gerek yoktur.
+    JavaScriptExecutor kullanmak icin komut dosyasinda(org.openqa.selenium.JavascriptExecutor) ice aktarmaniz yeterlidir.
+
+          executeScript araciligiyla sayfayi 1000 oiksel kaydirin.
+          Javascript tontemi ScrollBy(), web sayfasini belirli piksel sayisina kaydirir.
+
+          ScrollBy()  yontemlerinin sozdizimi soyledir:
+          executeScript("window.scrollBy(x-piksel,y-piksel)"); ya da js.executeScript("scroll(x,y);");
+          x-piksel x eksenindeki sayidir,sayi pozitifse sola,sayi negatifse saga hareket eder.
+          y-piksel y eksenindeki sayidir,sayi pozitifse asagidogru,sayi negatifse yukari dogru hareket eder.
+
+          Ornek:
+          js.executeScript("window.scrollBy(0,1000)");  // Dikey olarak 1000 piksel asagi kaydir.
+     */
+    /*=====================================================================================================================================================*/
+    // ...Exercise4...
+    // https://www.teknosa.com/ adresine gidiniz
+    // Search box senKeys "oppo" and keys Enter
+    // sonuc sayisini yazdiriniz
+    // cikan ilk urune tiklayiniz
+    // sepete ekleyiniz
+    // sepetime git 'e tiklayiniz
+    // consaol'da "Siparis Özeti" webelementinin text'ini yazdiriniz
+    // Alisverisi tamamlayiniz
+    // son olarak da "Teknosa'ya hos geldiniz" webelementinin text 'ini yazdiriniz
+    // driver'i kapatiniz
+
+}
