76ab3ae59d611d2e3ad10579c1d0894ca491c63d
burak <karanfilkaranfil822@gmail.com>
2022-09-22 09:39:55 +0300

    "23.09.22"

--- src/test/java/practise_Elf/C05.java
+++ src/test/java/practise_Elf/C05.java
@@ -1,10 +1,51 @@
 package practise_Elf;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
 public class C05 {
-    // ...Exercise5...
-    // @BeforeClass'in icine driver'i kuralim
-    // maximize edip tum web elementler yuklenene kadar 10sn bekletelim
-    // Google'a gidelim ve sayfa basliginin "Google" icerdigini dogrulayalim
-    // Amazon'a gidelim ve url in www.amazon.com icerip icermedigini dogrulayalim
-    // @AfterClass to driver'i kapatalim
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        // ...Exercise5...
+        // @BeforeClass'in icine driver'i kuralim
+        // maximize edip tum web elementler yuklenene kadar 10sn bekletelim
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        // @AfterClass to driver'i kapatalim
+        driver.close();
+    }
+
+    @Test
+    public void googleTest() {
+
+        // Google'a gidelim ve sayfa basliginin "Google" icerdigini dogrulayalim
+        driver.get("https://google.com");
+        String actualTitle = driver.getTitle();
+        String expectedTitle = "Google";
+        Assert.assertEquals(expectedTitle, actualTitle);
+    }
+    @Test
+    public void amazonTest() {
+        // Amazon'a gidelim ve url in www.amazon.com icerip icermedigini dogrulayalim
+       driver.get("https://www.amazon.com");
+        String actual=driver.getCurrentUrl();
+        String expected="www.amazon.com";
+        Assert.assertTrue(actual.contains(expected));
+
+    }
 }
--- src/test/java/practise_Elf/C06.java
+++ src/test/java/practise_Elf/C06.java
@@ -1,11 +1,62 @@
 package practise_Elf;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.lang.reflect.Array;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
 public class C06 {
-    // 1. Amazon.com'a gidelim
-    // driver.get("https://amazon.com/");
-    // 2. DropDown uzerinde Books secelim.(All yazan yerde)
-    //    kategorilerin hepsini konsola yazdiralim
-    // 3. Arama kutusuna Les Miserables yazalim ve arama yapalim.
-    // 4. Sonuc sayisini ekrana yazdiralim.
-    // 5. Sonuclarin Les Miserables i icerdigini assert edelim
+    static WebDriver driver;
+
+    @BeforeClass
+    public static void setUp() {
+        // ...Exercise5...
+        // @BeforeClass'in icine driver'i kuralim
+        // maximize edip tum web elementler yuklenene kadar 10sn bekletelim
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @AfterClass
+    public static void tearDown() {
+
+        driver.close();
+    }
+
+    @Test
+    public void amazonTest() {
+        // 1. Amazon.com'a gidelim
+        driver.get("https://amazon.com");
+
+        // 2. DropDown uzerinde Books secelim.(All yazan yerde)
+        WebElement ddm=driver.findElement(By.cssSelector("#searchDropdownBox"));
+        Select select=new Select(ddm);
+        select.selectByVisibleText("Books");
+        //    kategorilerin hepsini konsola yazdiralim
+        List<WebElement> ddmList=select.getOptions();
+        ddmList.forEach(t-> System.out.println(t.getText()));
+        // 3. Arama kutusuna Les Miserables yazalim ve arama yapalim.
+        driver.findElement(By.cssSelector("#twotabsearchtextbox")).sendKeys("Les Miserables", Keys.ENTER);
+        // 4. Sonuc sayisini ekrana yazdiralim.
+        WebElement sonuc=driver.findElement(By.xpath("//span[text()='1-16 of over 5,000 results for']"));
+       String[] sonucArr = sonuc.getText().split(" ");
+        System.out.println("sonuc sayisi ="+sonucArr[3]);
+        // 5. Sonuclarin Les Miserables i icerdigini assert edelim
+        String actual=driver.findElement(By.xpath("//span[text()='\"Les Miserables\"']")).getText();
+        Assert.assertTrue(actual.contains("Les Miserables"));
+    }
 }
